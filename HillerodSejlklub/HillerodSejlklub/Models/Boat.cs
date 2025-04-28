0a9191a4958f51809873c3ae948b421a794948ac
mak025 <mak025@edu.zealand.dk>
2025-04-28 23:17:05 +0200

    Adds boat properties and types
    Extends the Boat class with properties like material, color, weight, build year, name, registration, availability, maintenance log, location, and repair status.
    
    Introduces SailBoat and MotorBoat classes inheriting from Boat, adding specific properties for each type.
    
    Provides methods to manage maintenance, location, and details retrieval for boats.

--- HillerodSejlklub/HillerodSejlklub/Models/Boat.cs
+++ HillerodSejlklub/HillerodSejlklub/Models/Boat.cs
@@ -2,35 +2,112 @@
 {
     public abstract class Boat
     {
-        public string type { get; protected set; }
+        public string Type { get; protected set; }
+        public string Size { get; protected set; }
+        public double Seats { get; protected set; }
+        public string Material { get; protected set; }
+        public string Color { get; protected set; }
+        public double Weight { get; protected set; }
+        public int YearBuilt { get; protected set; }
+        public string Name { get; protected set; }
+        public string RegistrationNumber { get; protected set; }
+        public bool IsAvailable { get; protected set; } = true;
+        public List<string> MaintenanceLog { get; private set; } = new List<string>();
+        public string CurrentLocation { get; protected set; }
+        public DateTime LastMaintenanceDate { get; protected set; }
+        public bool NeedsRepair { get; protected set; } = false;
 
-        public string size { get; protected set; }
-
-        public double seats { get; protected set; }
-
-        public Boat(string type, string size, double seats)
+        public Boat(string type, string size, double seats, string material, string color, double weight, int yearBuilt, string name, string registrationNumber)
         {
-            this.type = type;
-            this.size = size;
-            this.seats = seats;
+            Type = type;
+            Size = size;
+            Seats = seats;
+            Material = material;
+            Color = color;
+            Weight = weight;
+            YearBuilt = yearBuilt;
+            Name = name;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public void AddMaintenanceLog(string logEntry)
+        {
+            MaintenanceLog.Add(logEntry);
+            LastMaintenanceDate = DateTime.Now;
+        }
+
+        public void MarkAsRepaired()
+        {
+            NeedsRepair = false;
+            AddMaintenanceLog("Boat repaired.");
+        }
+
+        public void UpdateLocation(string newLocation)
+        {
+            CurrentLocation = newLocation;
         }
 
         public string GetBoatDetails()
         {
-            return "Boat Type: " + type + "\n" +                   // This returns the details of the device brand in a string format.
-                   "Boat Size: " + size + "\n" +     // This returns the details of the device manufacturer in a string format.
-                   "Amount of seats: " + seats;
+            return $"Boat Type: {Type}\n" +
+                   $"Boat Size: {Size}\n" +
+                   $"Seats: {Seats}\n" +
+                   $"Material: {Material}\n" +
+                   $"Color: {Color}\n" +
+                   $"Weight: {Weight} kg\n" +
+                   $"Year Built: {YearBuilt}\n" +
+                   $"Name: {Name}\n" +
+                   $"Registration Number: {RegistrationNumber}\n" +
+                   $"Is Available: {IsAvailable}\n" +
+                   $"Current Location: {CurrentLocation}\n" +
+                   $"Needs Repair: {NeedsRepair}\n" +
+                   $"Last Maintenance Date: {LastMaintenanceDate.ToShortDateString()}";
         }
     }
 
     public class SailBoat : Boat
     {
-        private string Sails { get; set; }
+        public string SailType { get; private set; } // e.g., Main Sail, Jib, Spinnaker
+        public int NumberOfSails { get; private set; }
+        public bool HasKeel { get; private set; }    // Indicates if the sailboat has a keel
 
-        public SailBoat(string type, string size, double seats, string Sails)
-            : base(type, size, seats)
+        public SailBoat(string type, string size, double seats, string material, string color, double weight, int yearBuilt, string name, string registrationNumber, string sailType, int numberOfSails, bool hasKeel)
+            : base(type, size, seats, material, color, weight, yearBuilt, name, registrationNumber)
         {
-            this.Sails = Sails;
+            SailType = sailType;
+            NumberOfSails = numberOfSails;
+            HasKeel = hasKeel;
+        }
+
+        public string GetSailBoatDetails()
+        {
+            return GetBoatDetails() + "\n" +
+                   $"Sail Type: {SailType}\n" +
+                   $"Number of Sails: {NumberOfSails}\n" +
+                   $"Has Keel: {HasKeel}";
+        }
+    }
+
+    public class MotorBoat : Boat
+    {
+        public int EnginePower { get; private set; } // Engine power in horsepower (HP)
+        public string FuelType { get; private set; } // e.g., Gasoline, Diesel, Electric
+        public double FuelCapacity { get; private set; } // Fuel capacity in liters
+
+        public MotorBoat(string type, string size, double seats, string material, string color, double weight, int yearBuilt, int enginePower, string fuelType, double fuelCapacity)
+            : base(type, size, seats, material, color, weight, yearBuilt)
+        {
+            EnginePower = enginePower;
+            FuelType = fuelType;
+            FuelCapacity = fuelCapacity;
+        }
+
+        public string GetMotorBoatDetails()
+        {
+            return GetBoatDetails() + "\n" +
+                   $"Engine Power: {EnginePower} HP\n" +
+                   $"Fuel Type: {FuelType}\n" +
+                   $"Fuel Capacity: {FuelCapacity} liters";
         }
     }
 }
