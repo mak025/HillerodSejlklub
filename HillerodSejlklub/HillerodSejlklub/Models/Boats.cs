3078a20898844c798fcf1e508009b18090450c16
mak025 <mak025@edu.zealand.dk>
2025-04-28 12:28:55 +0100

    Adds Boats model with inheritance
    Creates an abstract base class for boats with common properties.
    Implements a SailBoats class inheriting from the base class,
    allowing for specific properties.

---
+++ HillerodSejlklub/HillerodSejlklub/Models/Boats.cs
@@ -0,0 +1,36 @@
+﻿namespace HillerodSejlklub.Models
+{
+    public abstract class Boats
+    {
+        public string type { get; protected set; }
+
+        public string size { get; protected set; }
+
+        public double seats { get; protected set; }
+
+        public Boats(string type, string size, double seats)
+        {
+            this.type = type;
+            this.size = size;
+            this.seats = seats;
+        }
+
+        public string getBoatDetails()
+        {
+            return "Boat Type: " + type + "\n" +                   // This returns the details of the device brand in a string format.
+                   "Boat Size: " + size + "\n" +     // This returns the details of the device manufacturer in a string format.
+                   "Amount of seats: " + seats;
+        }
+    }
+
+    public class SailBoats : Boats
+    {
+        private string Sails { get; set; }
+
+        public SailBoats(string type, string size, double seats, string Sails)
+            : base(type, size, seats)
+        {
+            this.Sails = Sails;
+        }
+    }
+}
