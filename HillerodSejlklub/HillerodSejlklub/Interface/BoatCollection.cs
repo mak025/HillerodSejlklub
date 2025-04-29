3e396cf49720f6428a51a499a3a86daca52eeacf
mak025 <mak025@edu.zealand.dk>
2025-04-29 12:50:11 +0200

    Adds a motorboat to the boat collection
    Adds a new motorboat instance to the boat collection
    for testing and demonstration purposes.

--- HillerodSejlklub/HillerodSejlklub/Interface/BoatCollection.cs
+++ HillerodSejlklub/HillerodSejlklub/Interface/BoatCollection.cs
@@ -18,6 +18,7 @@ namespace HillerodSejlklub.Interface
             _boats.Add(new SailBoat("sailboat", "small", 6, "fiberglas", "alle sammen", 80, 2012, "Jytte", "JYT50425035", "Main Sail", 1, true));
             _boats.Add(new SailBoat("sailboat", "small", 6, "fiberglas", "alle sammen", 80, 2012, "Jytte", "JYT50425036", "Main Sail", 1, true));
             _boats.Add(new SailBoat("sailboat", "small", 6, "fiberglas", "alle sammen", 80, 2012, "Jytte", "JYT50425037", "Main Sail", 1, true));
+            _boats.Add(new MotorBoat("Motorboat", "medium", 12, "fiberglass", "blue", 125, 2010, "Fatty", "FAT23598523", 105, "Potatoes", 6));
         } //test
     }
 }
