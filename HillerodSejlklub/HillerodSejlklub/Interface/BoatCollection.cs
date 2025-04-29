b0a4874abc7e8bbf584f69808f162ed9bb78e79a
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-04-29 10:12:00 +0200

    boats!!

--- HillerodSejlklub/HillerodSejlklub/Interface/BoatCollection.cs
+++ HillerodSejlklub/HillerodSejlklub/Interface/BoatCollection.cs
@@ -1,6 +1,23 @@
-﻿namespace HillerodSejlklub.Interface
+﻿using HillerodSejlklub.Models;
+
+namespace HillerodSejlklub.Interface
 {
-    public class BoatCollection
+    public class BoatCollection : IBoat
     {
+        private List<Boat> _boats;
+
+        public BoatCollection()
+        {
+            _boats = new List<Boat>();
+            Seed();
+        }
+
+        private void Seed()
+        {
+            _boats.Add(new SailBoat("sailboat", "small", 6, "fiberglas", "alle sammen", 80, 2012, "Jytte", "JYT50425034", "Main Sail", 1, true));
+            _boats.Add(new SailBoat("sailboat", "small", 6, "fiberglas", "alle sammen", 80, 2012, "Jytte", "JYT50425035", "Main Sail", 1, true));
+            _boats.Add(new SailBoat("sailboat", "small", 6, "fiberglas", "alle sammen", 80, 2012, "Jytte", "JYT50425036", "Main Sail", 1, true));
+            _boats.Add(new SailBoat("sailboat", "small", 6, "fiberglas", "alle sammen", 80, 2012, "Jytte", "JYT50425037", "Main Sail", 1, true));
+        }
     }
 }
